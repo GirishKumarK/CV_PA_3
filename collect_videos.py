5c46091919c852ab76b5ccc4d601b49edbd19d8a
Girish Kumar Kannan <girishkumar.kannan@gmail.com>
2017-11-26 23:26:56 -0500

    Add files via upload

---
+++ collect_videos.py
@@ -0,0 +1,50 @@
+import os
+from shutil import copy2
+import numpy as np
+import pandas as pd
+
+# Create an Action-Label List to Save the Video-Label Pairs
+axn_lbl_list = []
+
+# Create a Destination Directory for All Videos
+dst_dir = '.\\action_videos'
+if not os.path.exists(dst_dir):
+    print ('~' * 100)
+    print ('Creating Destination Directory ...')
+    os.makedirs(dst_dir)
+    print ('Destination Directory Created !')
+    print ('Copying Video Files ...')
+else:
+    print ('~' * 100)
+    print ('Destination Directory Already Exists.')
+    print ('Copying Video Files ...')
+
+# Create a Copy of All Given Videos into the Directory
+rootDir = '.\\ucf_sports_actions\\ucf action'
+for dirName, subdirList, fileList in os.walk(rootDir, topdown=True):
+    for fname in fileList:
+        if fname.endswith('.avi'):
+            print ('~' * 100)
+            src = os.path.join(dirName, fname)
+            print ('Copying Video File From : ' + src)
+            dst = os.path.join(dst_dir, fname)
+            print ('Copying Video File To   : ' + dst)
+            copy2(src, dst)
+            print ('File Copied Successfully !')
+            # fname = action and split = label
+            axn_lbl_list.append([fname, src.split('\\')[3]])
+
+# Convert the Final Action-Label List to NumPy Array
+axn_lbl_list = np.array(axn_lbl_list).astype(str).reshape(len(axn_lbl_list), 2)
+df = pd.DataFrame(axn_lbl_list)
+csv_name = os.path.join(dst_dir, 'action_label.csv')
+# Save the Action-Label List as CSV File
+df.to_csv(csv_name, header=['Action', 'Label'], index=False)
+
+# Indicate Copy Process Completion
+print ('~' * 100)
+print ('All Video Files Copied Successfully !')
+print ('~' * 100)
+
+
+# End of File
---
+++ make_videos.py
@@ -0,0 +1,87 @@
+import os
+import numpy as np
+import skimage.io as skiio
+import skvideo.io as skvio
+
+
+# Main Directory
+main_dir = '.\\ucf_sports_actions\\ucf action'
+
+# Actions Directory
+axns_dir = os.listdir(main_dir)
+
+# Indicate Video Creation Process
+print ('~' * 100)
+print ('Creating Videos For Main Directory : ' + main_dir)
+
+# For All Root Directories
+for ad in axns_dir:
+    
+    # Get Current Action Directory
+    curr_axn_dir = os.path.join(main_dir, ad)
+    
+    # Get Current Action Set Directory
+    axns_set = os.listdir(curr_axn_dir)
+    
+    # Indicate Video Creation Process
+    print ('~' * 100)
+    print ('Creating Videos For Action Directory : ' + curr_axn_dir)
+    
+    # Generate Videos for All Action Sets in the Directory
+    for aset in axns_set:
+        current_dir = os.path.join(curr_axn_dir, aset)
+        files = os.listdir(current_dir)
+        
+        # Get a list of all JPG files
+        jpgs = []
+        for file in files:
+            if not os.path.isdir(file):
+                if file.endswith('.jpg'):
+                    jpgs.append(os.path.join(current_dir, file))
+        
+        # A Flag to Indicate No JPG Files
+        jpg_flag = False
+        
+        # Create Video for All Folders with JPG files
+        if (len(jpgs) != 0):
+            jpg_flag = True
+            
+            # Indicate Video Creation Process
+            print ('~' * 100)
+            print ('Generating Video For Action Root Directory : ' + current_dir)
+            
+            # Open Images in JPG list
+            images, img_dims = [], []
+            for jpg in jpgs:
+                image = skiio.imread(jpg)
+                height, width, channels = image.shape
+                images.append(image)
+                img_dims.append([height, width, channels])
+            images = np.array(images)
+            img_dims = np.array(img_dims)
+            
+            # Merge the Images to one AVI file
+            video_name = (jpgs[0].split('.')[1].split('\\')[-1] + '.avi')
+            video_path = os.path.join(current_dir, video_name)
+            skvio.vwrite(video_path, images, inputdict={'-r':'10'})
+            
+            # Indicate Video Generation Completion
+            print ('Video Created For Action Root Directory : ' + current_dir)
+            
+    if (jpg_flag == True):
+        # Indicate Video Generation Completion
+        print ('~' * 100)
+        print ('Videos Created For Action Directory : ' + curr_axn_dir)
+    else:
+        # Indicate No JPG Files Found
+        print ('~' * 100)
+        print ('No JPG Files Found In Action Directory : ' + curr_axn_dir)
+        print ('No Videos Created For Action Directory : ' + curr_axn_dir)
+
+# Indicate Video Generation Completion
+print ('~' * 100)
+print ('All Videos Generated Successfully !')
+print ('~' * 100)
+
+
+# End of File
